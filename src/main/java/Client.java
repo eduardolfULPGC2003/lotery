87dc224a6bea0f32ec9cdefeef0c25dc9c245a8b
eduardolfULPGC2003 <eduardolf2003@gmail.com>
2023-11-28 16:38:35 +0100

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,109 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Client {
+    private String email;
+
+    public Client(String email) {
+        this.email = email;
+    }
+
+    private void play() throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lets play!");
+        LocalDateTime dateTime = getDate();
+        System.out.println("Please introduce a number between 0-255: ");
+        int number = scanner.nextInt();
+         if (dateTime.isBefore(LocalDateTime.now())) {
+             System.out.println("Wrong Date");
+             System.out.println("----------------------------------------------");
+        } else if (number > 255 | number < 0) {
+             System.out.println("Wrong Number. Must be between 0 and 255");
+             System.out.println("----------------------------------------------");
+        } else {
+             String message = this.email + "|" + dateTime.toString() + "|" + number;
+             String reply = request(message);
+             System.out.println(reply);
+        }
+    }
+
+    private LocalDateTime getDate(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("When do you want to play? (Introduce de number in the format DD/MM/YY)");
+        String date = scanner.nextLine();
+        try{
+            LocalDate parsedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yy"));
+            System.out.println("Now introduce de time in the format: HH:MM (Note that we play every hour at :30)");
+            String time = scanner.nextLine();
+            LocalTime parsedTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm")).withMinute(30);
+            LocalDateTime dateTime = parsedDate.atTime(parsedTime);
+            return dateTime;
+        } catch (DateTimeParseException pe){
+            System.out.println("Incorrect format: "+ pe);
+            System.out.println("----------------------------------------------");
+            System.exit(0);
+        }
+        return null;
+    }
+
+    private String request(String message){
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket();
+            byte[] buffer = message.getBytes();
+            DatagramPacket req = new DatagramPacket(buffer, buffer.length,
+                    InetAddress.getByName("localhost"), Server.PORT);
+            aSocket.send(req);
+            buffer = new byte[1024];
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            aSocket.receive(reply);
+            String receivedMessage = new String(reply.getData(), 0, reply.getLength());
+            return receivedMessage;
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        } finally {
+            if(aSocket != null) aSocket.close();
+        }
+        return null;
+    }
+
+    public static void main(String args[]) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Lottery365. Bet whenever you want");
+        System.out.print("Please enter your email: ");
+        String regex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        String email = scanner.nextLine();
+        if (!email.matches(regex)){
+            System.out.println("Wrong email. Try again.");
+            System.exit(0);
+        }
+        Client client = new Client(email);
+        mainLoop:
+        while (true){
+            System.out.println("What do you want to do?\n1. Play\n2. Get winners\n3. Exit");
+            System.out.print("Your selection: ");
+            int number = scanner.nextInt();
+            switch (number){
+                case 1:
+                    client.play();
+                    break;
+                case 2:
+                    //TODO
+                    break;
+                case 3:
+                    System.out.println("Good bye!");
+                    break mainLoop;
+            }
+        }
+    }
+}
+
---
+++ src/main/java/Server.java
@@ -0,0 +1,129 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Server {
+    public static final int PORT = 6789;
+    private int pool = 0;
+    private String path;
+
+    public Server(String path, int pool) throws CsvValidationException {
+        this.pool = pool;
+        this.path = path;
+        this.start();
+    }
+
+    private void start() throws CsvValidationException {
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket(PORT);
+            byte[] buffer = new byte[1024];
+
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    if (LocalDateTime.now().getMinute()==37) {
+                        System.out.println("Ahora");
+                        try {
+                            System.out.println(lottery());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        } catch (CsvValidationException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }, 0, 60000);
+
+            while (true) {
+                DatagramPacket req = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(req);
+                String participation = new String(req.getData(), 0, req.getLength());
+                String response = accept(participation);
+                buffer = response.getBytes();
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length,
+                        req.getAddress(), req.getPort());
+                aSocket.send(reply);
+
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        } finally {
+        }
+    }
+
+    private String accept(String req) throws IOException, CsvValidationException {
+        String[] parts = req.split("\\|");
+        String fileName = parts[1].replace(":", "-") + ".csv";
+        File file = new File(this.path, fileName);
+        if (file.createNewFile()){
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+            writer.write(parts[0] + "|" + parts[2] + ",");
+            writer.close();
+        }
+        else {
+            String path = this.path+"/"+parts[1].replace(":","-")+".csv";
+            CSVReader reader = new CSVReader(new FileReader(path));
+            String[] shares;
+            while ((shares=reader.readNext()) != null) {
+                for (String share : shares) {
+                    System.out.println(share);
+                    String[] parts_share = share.split("\\|");
+                    if (parts_share[0].equals(parts[0]) && parts_share[1].equals(parts[2]))
+                        return "You can't play twice with the same number";
+                }
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+            writer.write(parts[0] + "|" + parts[2] + ",");
+            writer.close();
+        }
+        pool += 100;
+        return "Thank you for participating: " + req;
+    }
+
+    private String lottery() throws IOException, CsvValidationException {
+        List<String> winners = new ArrayList<>();
+        Integer winner_num = new Random().nextInt(256);
+        String now = LocalDateTime.now().withMinute(30).toString();
+        String path_win = this.path+"/" + now.replace(":","-").substring(0,16) + ".csv";
+        try {
+            CSVReader reader = new CSVReader(new FileReader(path_win));
+            String[] shares;
+            while ((shares = reader.readNext()) != null) {
+                for (String share : shares) {
+                    System.out.println(share);
+                    String[] parts_share = share.split("\\|");
+                    if (parts_share.length == 2) {
+                        if (Integer.parseInt(parts_share[1]) == winner_num)
+                            winners.add(parts_share[0]);
+                    }
+                }
+            }
+            if (winners.size() == 0) return "Number: " + winner_num + ". No winners";
+            else {
+                int ppw = pool / winners.size();
+                File file = new File(this.path + "/winners.csv");
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+                for (String winner : winners) {
+                    writer.write(now.substring(0, 16) + "|" + winner + "|" + ppw + ",");
+                }
+                writer.close();
+                pool = 0;
+                return "The winner number is " + winner_num + ". The winners are" + winners.toString() + ". Each one won:" + ppw;
+            }
+        } catch (FileNotFoundException ie){
+            return "No participants";
+        }
+    }
+
+    public static void main(String args[]) throws CsvValidationException {
+        new Server("files", 200);
+    }
+}
